92e05cccf666dedeb7d4b3b6535ec4dff4efcb02
Nigel <nmedfort@sfu.ca>
2023-08-28 18:07:20 -0700

    NON-FUNCTIONAL! initial checkin

---
+++ tools/csv_validator/csv_validator.cpp
@@ -0,0 +1,415 @@
+/*
+ *  Copyright (c) 2018 International Characters.
+ *  This software is licensed to the public under the Open Software License 3.0.
+ *  icgrep is a trademark of International Characters.
+ */
+
+#include <kernel/core/idisa_target.h>
+#include <boost/filesystem.hpp>
+#include <re/cc/cc_compiler.h>
+#include <re/cc/cc_compiler_target.h>
+#include <re/adt/adt.h>
+#include <re/unicode/resolve_properties.h>
+#include <unicode/utf/utf_compiler.h>
+#include <kernel/core/kernel_builder.h>
+#include <kernel/pipeline/pipeline_builder.h>
+#include <kernel/basis/s2p_kernel.h>
+#include <kernel/io/source_kernel.h>
+#include <kernel/core/streamset.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Module.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/raw_ostream.h>
+#include <pablo/pablo_kernel.h>
+#include <pablo/builder.hpp>
+#include <pablo/pe_zeroes.h>
+#include <pablo/pablo_toolchain.h>
+#include <kernel/pipeline/driver/cpudriver.h>
+#include <toolchain/toolchain.h>
+#include <fileselect/file_select.h>
+#include <fcntl.h>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <sys/stat.h>
+#include <vector>
+#ifdef ENABLE_PAPI
+#include <util/papi_helper.hpp>
+#endif
+
+namespace fs = boost::filesystem;
+
+using namespace llvm;
+using namespace codegen;
+
+static cl::OptionCategory wcFlags("Command Flags", "csv validator options");
+
+static cl::list<std::string> inputFiles(cl::Positional, cl::desc("<input file ...>"), cl::OneOrMore, cl::cat(wcFlags));
+
+bool strictRFC4180 = false;
+
+
+std::vector<fs::path> allFiles;
+
+
+
+using namespace pablo;
+using namespace kernel;
+using namespace cc;
+using namespace re;
+
+
+
+class StructureIdentifier final: public pablo::PabloKernel {
+public:
+    StructureIdentifier(BuilderRef b, StreamSet * const text, StreamSet * const breaks, StreamSet * const deletionFollowsMask);
+protected:
+    void generatePabloMethod() override;
+private:
+    std::vector<char> Quotes = {'"', '\''};
+    std::vector<char> FieldBreak = {','};
+    std::vector<char> RecordBreak = {'\n'};
+    char Escape = '\\';
+};
+
+StructureIdentifier::StructureIdentifier (BuilderRef b, StreamSet * const text, StreamSet * const breaks, StreamSet * const deletionFollowsMask)
+: PabloKernel(b, "csv_structure", // make name depend on options
+    {Bind("text", text)},
+    {Bind("breaks", breaks), Bind("deletionFollows", deletionFollowsMask)},
+    {},
+    {}) {
+    assert (breaks->getNumElements() == 2);
+    assert (deletionFollowsMask->getNumElements() == 1);
+}
+
+void StructureIdentifier::generatePabloMethod() {
+    PabloBuilder pb(getEntryScope());
+    Parabix_CC_Compiler_Builder ccc(getEntryScope(), getInputStreamSet("text"));
+
+
+    const auto n = Quotes.size();
+
+    std::vector<Var *> quoteMark(n);
+
+    for (unsigned i = 0; i < n; ++i) {
+        quoteMark[i] = pb.createVar("q", ccc.compileCC(re::makeByte(Quotes[i])));
+    }
+
+    PabloAST * alternatingZeroOne = pb.createRepeat(2, 0b10);
+    PabloAST * alternatingOneZero = pb.createNot(alternatingZeroOne);
+
+    PabloAST * zeroes = pb.createZeroes();
+
+    bool hasEscapeChar = !strictRFC4180;
+
+
+    std::vector<Var *> followsFilteredChar(n + (hasEscapeChar ? 1 : 0));
+
+    auto identifyMarkAfterOddLengthRuns = [&](PabloBuilder & it, PabloAST * first, PabloAST * span, const unsigned index) -> PabloAST * {
+        PabloAST * negFirst = it.createAnd(first, alternatingZeroOne);
+        PabloAST * markAfterNegRun = it.createScanThru(negFirst, span);        
+        PabloAST * markAfterOddLengthNegRun = it.createAnd(markAfterNegRun, alternatingOneZero);
+
+        PabloAST * negMarked = it.createIntrinsicCall(pablo::Intrinsic::SpanUpTo, {negFirst, it.createNot(span)});
+        negMarked = it.createAnd(negMarked, alternatingOneZero);
+
+        PabloAST * posFirst = it.createAnd(first, alternatingOneZero);
+        PabloAST * markAfterPosRun = it.createScanThru(posFirst, span);
+        PabloAST * markAfterOddLengthPosRun = it.createAnd(markAfterPosRun, alternatingZeroOne);
+
+        PabloAST * posMarked = it.createIntrinsicCall(pablo::Intrinsic::SpanUpTo, {posFirst, it.createNot(span)});
+        posMarked = it.createAnd(posMarked, alternatingZeroOne);
+
+        // Although not permited by RFC 4180, suppose both ' and " are valid quote characters.  If we mark
+        // all of the open/close quotes and escape characters for deletion, we run into a problem. Ideally,
+        // we want:
+
+        //     TEXT = ''''     TEXT='"'''     TEXT=' "'' '      TEXT='""',"''"
+        //            ^ ^           ^ ^ ^          ^  ^  ^           ^  ^ ^  ^
+
+        // But we don't have enough information to decide that here. The best we can do is try and decide
+        // which characters we may want to keep, filter out the false matches, and do a lookahead later.
+        // Thus we end up with something like:
+
+        //     TEXT = ''''     TEXT='"'''     TEXT=' "'' '      TEXT='""',"''"
+        //             ^ ^           ^*^ ^          ^ *^  ^           ^ *^ ^ *^
+
+
+        Var * var = pb.createVar("followsFiltered", zeroes);
+        followsFilteredChar[index] = var;
+        it.createAssign(var, it.createOr(negMarked, posMarked));
+
+        return it.createOr(markAfterOddLengthNegRun, markAfterOddLengthPosRun);
+    };
+
+    if (!strictRFC4180) {
+        PabloAST * escapeMark = ccc.compileCC(re::makeByte(Escape));
+
+        auto it = pb.createScope();
+        pb.createIf(escapeMark, it);
+
+        // Look through runs of ///...// to see if there are an odd number of them. If so, the char after
+        // the last one is escaped out.
+
+        PabloAST * first = it.createAnd(it.createNot(it.createAdvance(escapeMark, 1)), escapeMark);
+        PabloAST * nonEscapedChars = it.createNot(identifyMarkAfterOddLengthRuns(it, first, escapeMark, n));
+
+        for (unsigned i = 0; i < n; ++i) {
+            PabloAST * q = it.createAnd(quoteMark[i], nonEscapedChars);
+            it.createAssign(quoteMark[i], q);
+        }
+
+    }
+
+    std::vector<Var *> followsQuote(n);
+    std::vector<Var *> advancedQuote(n);
+    for (unsigned i = 0; i < n; ++i) {
+        followsQuote[i] = pb.createVar("followsQuote", zeroes);
+        advancedQuote[i] = pb.createVar("advQuote", zeroes);
+    }
+
+    Var * allQuoteFollows = pb.createVar("allFollows", zeroes);
+
+    for (unsigned i = 0; i < n; ++i) {
+        // similar to above, we want to filter out adjacent quote marks. Unlike the / escapes, we will end up being
+        // one position after the quote we're interested in. So if we have:
+
+        // TEXT     = """ """""
+        // QM       = ...1.....1
+
+        // Ideal QM = 1.......1.
+
+        // This is problematic since what we consider the actual quote mark as opposed to the position after mark
+        // is dependent on whether we're starting or closing a mark span. We fix that in a subsequent stage
+        // by and only focus on finding the non-escaped quote marks
+
+        auto quoteScope = pb.createScope();
+        PabloAST * q = quoteMark[i];
+        pb.createIf(q, quoteScope);
+        PabloAST * advQ = quoteScope.createAdvance(q, 1);
+        quoteScope.createAssign(advancedQuote[i], advQ);
+
+        PabloAST * first = quoteScope.createAnd(quoteScope.createNot(advQ), q);
+        quoteScope.createAssign(followsQuote[i], identifyMarkAfterOddLengthRuns(quoteScope, first, q, i));
+        if (n > 1) {
+            quoteScope.createAssign(allQuoteFollows, quoteScope.createOr(first, followsQuote[i]));
+        }
+    }
+
+
+
+    // Although not permited by RFC 4180, suppose both ' and " are valid quote characters. We may have
+    // TEXT pattern such as '"""' , which should be considered a field of three double quotes. A more
+    // problematic example would be "'",'"''' .
+
+    // To consider "mixed" runs like that, we need to iterate sequentially through spans to decide what
+    // type of span the "outermost" one is and remove any non-matching follows that precede the next
+    // non-escaped matching one.
+
+    // TODO: check if we have any mixed types. even if the validator supports multiple quote mark types
+    // its unlikely that a document mixes them this pathelogical fashion.
+
+
+
+    // ----------------------------------------------------------------------------------------------
+
+    std::vector<Var *> followStarts(n);
+    std::vector<Var *> followEnds(n);
+    for (unsigned i = 0; i < n; ++i) {
+        followStarts[i] = pb.createVar("starts", zeroes);
+        followEnds[i] = pb.createVar("ends", zeroes);
+    }
+
+
+    // get the first unprocessed follows? better than doing the insert first trick and scanning through to "here"?
+    PabloAST * start = pb.createNot(pb.createAdvance(pb.createNot(zeroes), 1));
+    PabloAST * first = pb.createScanTo(start, allQuoteFollows);
+
+    Var * anyOpener = pb.createVar("anyOpener", first);
+
+    auto filterScope = pb.createScope();
+    pb.createWhile(anyOpener, filterScope);
+    // ----------------------------------------------------------------------------------------------
+    Var * next = filterScope.createVar("next", zeroes);
+    for (unsigned i = 0; i < n; ++i) {
+        auto check = filterScope.createScope();
+        PabloAST * s = filterScope.createAnd(first, followsQuote[i]);
+        pb.createIf(s, check);
+        // ----------------------------------------------------------------------------------------------
+        PabloAST * t = check.createAdvanceThenScanTo(s, followsQuote[i]);
+        check.createAssign(followStarts[i], check.createOr(followStarts[i], s));
+        check.createAssign(followEnds[i], check.createOr(followEnds[i], t));
+        // ----------------------------------------------------------------------------------------------
+
+        check.createAssign(next, t);
+    }
+    PabloAST * nextOpener = filterScope.createAdvanceThenScanTo(next, allQuoteFollows);
+    filterScope.createAssign(anyOpener, nextOpener);
+    // ----------------------------------------------------------------------------------------------
+
+    Var * unquoted = pb.createVar("unquotedText", pb.createNot(zeroes));
+
+    for (unsigned i = 0; i < n; ++i) {
+        auto check = pb.createScope();
+        pb.createIf(followStarts[i], check);
+        // ----------------------------------------------------------------------------------------------
+        PabloAST * spans = check.createIntrinsicCall(pablo::Intrinsic::SpanUpTo, {followStarts[i], followEnds[i]});
+        PabloAST * unfiltered = check.createNot(spans);
+
+
+        // remove any deletion follows from streams that are enclosed by a different type of quotes
+        for (unsigned j = 0; j < i; ++j) {
+            Var * f = followsFilteredChar[j];
+            check.createAssign(f, check.createAnd(f, unfiltered));
+        }
+        for (unsigned j = i + 1; j < n; ++j) {
+            Var * f = followsFilteredChar[j];
+            check.createAssign(f, check.createAnd(f, unfiltered));
+        }
+        check.createAssign(unquoted, check.createOr(unquoted, unfiltered));
+        // ----------------------------------------------------------------------------------------------
+    }
+
+    for (unsigned i = 1; i < followsFilteredChar.size(); ++i) {
+        pb.createAssign(followsFilteredChar[0], pb.createOr(followsFilteredChar[0], followsFilteredChar[i]));
+    }
+
+    Var * deletion = getOutput(1);
+    pb.createAssign(pb.createExtract(deletion, 0), followsFilteredChar[0]);
+
+
+    Var * breaks = getOutput(0);
+
+    CC * fieldBreakCC = re::makeByte(FieldBreak[0]);
+    for (unsigned i = 1; i < n; ++i) {
+        fieldBreakCC->insert(FieldBreak[i]);
+    }
+
+    PabloAST * fieldBreakChars = ccc.compileCC(fieldBreakCC);
+    pb.createAssign(pb.createExtract(breaks, 0), pb.createAnd(fieldBreakChars, unquoted));
+
+    CC * recordBreakCC = re::makeByte(RecordBreak[0]);
+    for (unsigned i = 1; i < n; ++i) {
+        recordBreakCC->insert(RecordBreak[i]);
+    }
+
+    PabloAST * recordBreakChars = ccc.compileCC(recordBreakCC);
+    pb.createAssign(pb.createExtract(breaks, 1), pb.createAnd(recordBreakChars, unquoted));
+
+}
+
+class PullBackOne final: public pablo::PabloKernel {
+public:
+    PullBackOne(BuilderRef b, StreamSet * const input, StreamSet * const output);
+protected:
+    void generatePabloMethod() override;
+};
+
+PullBackOne::PullBackOne (BuilderRef b, StreamSet * const input, StreamSet * const output)
+: PabloKernel(b, "csv_structure", // make name depend on options
+    {Bind("input", input, LookAhead(1))},
+    {Bind("output", output)},
+    {},
+    {}) {
+    assert (input->getNumElements() == 1);
+    assert (output->getNumElements() == 1);
+}
+
+void PullBackOne::generatePabloMethod() {
+    PabloBuilder pb(getEntryScope());
+    pb.createAssign(pb.createExtract(getOutput(0), 0), pb.createLookahead(getInput(0), 1));
+}
+
+
+typedef void (*WordCountFunctionType)(uint32_t fd, uint32_t fileIdx);
+
+WordCountFunctionType wcPipelineGen(CPUDriver & pxDriver) {
+
+    auto & iBuilder = pxDriver.getBuilder();
+
+    Type * const int32Ty = iBuilder->getInt32Ty();
+
+    auto P = pxDriver.makePipeline({Binding{int32Ty, "fd"}, Binding{int32Ty, "fileIdx"}});
+
+    Scalar * const fileDescriptor = P->getInputScalar("fd");
+    Scalar * const fileIdx = P->getInputScalar("fileIdx");
+
+    StreamSet * const ByteStream = P->CreateStreamSet(1, 8);
+
+    Kernel * mmapK = P->CreateKernelCall<ReadSourceKernel>(fileDescriptor, ByteStream);
+
+    auto BasisBits = P->CreateStreamSet(8, 1);
+    P->CreateKernelCall<S2PKernel>(ByteStream, BasisBits);
+
+    auto breaks = P->CreateStreamSet(2, 1);
+    auto delFollows = P->CreateStreamSet(1, 1);
+
+    P->CreateKernelCall<StructureIdentifier>(BasisBits, breaks, delFollows);
+
+    auto deletions = P->CreateStreamSet(1, 1);
+    P->CreateKernelCall<PullBackOne>(BasisBits, delFollows, deletions);
+
+    // Do we need to delete all the data from the basis bits and derived streams? It might be better
+    // if we can treat them as skippable characters in the RE matchers. It would be easy to have optional
+    // quote starts but if we don't enforce that a field is either quoted or unquoted entirely (e.g.,
+    // treat "123"45"67" as illegal, as per RFC 4180), then this becomes far more difficult. Further,
+    // if we allow explicit escaped chars using / instead of only considering "", then we'd have
+    // to fall back to deleting all of them.
+
+    return reinterpret_cast<WordCountFunctionType>(P->compile());
+}
+
+void wc(WordCountFunctionType fn_ptr, const uint32_t fileIdx) {
+    std::string fileName = allFiles[fileIdx].string();
+    struct stat sb;
+    const int fd = open(fileName.c_str(), O_RDONLY);
+    if (LLVM_UNLIKELY(fd == -1)) {
+        if (errno == EACCES) {
+            std::cerr << "wc: " << fileName << ": Permission denied.\n";
+        }
+        else if (errno == ENOENT) {
+            std::cerr << "wc: " << fileName << ": No such file.\n";
+        }
+        else {
+            std::cerr << "wc: " << fileName << ": Failed.\n";
+        }
+        return;
+    }
+    if (stat(fileName.c_str(), &sb) == 0 && S_ISDIR(sb.st_mode)) {
+        std::cerr << "wc: " << fileName << ": Is a directory.\n";
+        close(fd);
+        return;
+    }
+    fn_ptr(fd, fileIdx);
+    close(fd);
+}
+
+int main(int argc, char *argv[]) {
+    codegen::ParseCommandLineOptions(argc, argv, {&wcFlags, pablo_toolchain_flags(), codegen::codegen_flags()});
+    if (argv::RecursiveFlag || argv::DereferenceRecursiveFlag) {
+        argv::DirectoriesFlag = argv::Recurse;
+    }
+    CPUDriver pxDriver("csvv");
+
+    allFiles = argv::getFullFileList(pxDriver, inputFiles);
+
+
+    auto wordCountFunctionPtr = wcPipelineGen(pxDriver);
+
+    #ifdef REPORT_PAPI_TESTS
+    papi::PapiCounter<4> jitExecution{{PAPI_L3_TCM, PAPI_L3_TCA, PAPI_TOT_INS, PAPI_TOT_CYC}};
+    jitExecution.start();
+    #endif
+
+    for (unsigned i = 0; i < fileCount; ++i) {
+        wc(wordCountFunctionPtr, i);
+    }
+
+
+    #ifdef REPORT_PAPI_TESTS
+    jitExecution.stop();
+    jitExecution.write(std::cerr);
+    #endif
+
+    return 0;
+}
