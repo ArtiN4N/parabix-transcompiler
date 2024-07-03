9d4858d1cbaba4b24685234f791a4b3f8a7653cb
Julian <julian.mjm.300@gmail.com>
2024-07-03 04:12:59 -0700

    debugging latin half to full transform

--- tools/util/latinhalftofull.cpp
+++ tools/util/latinhalftofull.cpp
@@ -226,11 +226,11 @@ HexLinesFunctionType generatePipeline(CPUDriver & pxDriver) {
     // set to an output stream set, to positions marked by 1s in the first
     // argument (the spread mask).   Zeroes are inserted everywhere else.
     // This function performs STEP 1 in the comments above.
-    StreamSet * spreadBasis1 = P->CreateStreamSet(8);
-    SpreadByMask(P, hexInsertMask, BasisBits, spreadBasis1);
+    //StreamSet * spreadBasis1 = P->CreateStreamSet(8);
+    //SpreadByMask(P, hexInsertMask, BasisBits, spreadBasis1);
 
     StreamSet * spreadBasis = P->CreateStreamSet(8);
-    SpreadByMask(P, hexInsertMask, spreadBasis1, spreadBasis);
+    SpreadByMask(P, hexInsertMask, BasisBits, spreadBasis);
     SHOW_BIXNUM(spreadBasis);
 
     // Perform the logic of the Hexify kernel.
