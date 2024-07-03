2258f5d21c85410dedb4ffb34f06982da3891d96
Julian <julian.mjm.300@gmail.com>
2024-07-03 03:51:19 -0700

    debugging latin half to full transform

--- tools/util/latinhalftofull.cpp
+++ tools/util/latinhalftofull.cpp
@@ -1,80 +1,55 @@
 #include <kernel/core/kernel.h>
 #include <kernel/io/source_kernel.h>
-#include <kernel/io/stdout_kernel.h>  
+#include <kernel/io/stdout_kernel.h>
 #include <kernel/core/kernel_builder.h>
 #include <kernel/core/streamset.h>
 #include <kernel/pipeline/driver/cpudriver.h>
 #include <kernel/pipeline/pipeline_builder.h>
 #include <llvm/Support/CommandLine.h>
 #include <llvm/Support/raw_ostream.h>
-#include <llvm/IR/Module.h>
-#include <llvm/ADT/StringRef.h>
-#include <unicode/algo/decomposition.h>
-#include <unicode/core/unicode_set.h>
-#include <unicode/data/PropertyAliases.h>
-#include <unicode/data/PropertyObjects.h>
-#include <unicode/data/PropertyObjectTable.h>
-#include <unicode/utf/utf_compiler.h>
-#include <unicode/utf/transchar.h>
-#include <toolchain/toolchain.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-#define SHOW_STREAM(name) if (codegen::EnableIllustrator) P->captureBitstream(#name, name)
-#define SHOW_BIXNUM(name) if (codegen::EnableIllustrator) P->captureBixNum(#name, name)
-#define SHOW_BYTES(name) if (codegen::EnableIllustrator) P->captureByteData(#name, name)
 
 using namespace kernel;
 using namespace llvm;
 using namespace codegen;
 
-class UppercaseKernel : public MultiBlockKernel {
+class InsertExtraAKernel : public MultiBlockKernel {
 public:
-    UppercaseKernel(KernelBuilder &b, StreamSet *inputStream, StreamSet *outputStream)
-    : MultiBlockKernel(b, "UppercaseKernel",
+    InsertExtraAKernel(KernelBuilder &b, StreamSet *inputStream, StreamSet *outputStream)
+    : MultiBlockKernel(b, "InsertExtraAKernel",
         {Binding{"inputStream", inputStream}}, // input bindings
         {Binding{"outputStream", outputStream}}, // output bindings
         {}, // internal scalar bindings
         {}, // initializer bindings
-        {}) // kernel state bindings)
+        {}) // kernel state bindings
     {}
-
 
 protected:
     void generateMultiBlockLogic(KernelBuilder &b, llvm::Value * const numOfBlocks) override {
-        // bitBlockType is obtained from the KernelBuilder. 
-        // This type represents the SIMD width, typically 128 or 256 bits
-        Type * const bitBlockType = b.getBitBlockType(); // 
+        // bitBlockType represents the SIMD width, typically 128 or 256 bits
+        Type * const bitBlockType = b.getBitBlockType();
+        
+        // Load input stream block
+        Value * inputBlock = b.loadInputStreamBlock("inputStream", b.getInt32(0), b.getInt32(0));
 
-        // Load input and apply the uppercase transformation
-        for (unsigned i = 0; i < b.getBitBlockWidth(); i += bitBlockType->getPrimitiveSizeInBits()) {
-            // Load input block
-            Value * inputBlock = b.loadInputStreamBlock("inputStream", b.getInt32(0), b.getInt32(i));
+        // Define the mask for 'a' characters
+        Value * isA = b.CreateICmpEQ(inputBlock, b.getInt8('a'));
 
-            // Mask to identify lowercase letters (0x20)
-            Value * lowercaseMask = b.CreateVectorSplat(bitBlockType->getPrimitiveSizeInBits() / 8, b.getInt8(0x20));
-            llvm::errs() << "lowerCaseMask: " << *lowercaseMask << "\n"; // for testing
-            llvm::errs() << "bitBlockType: " << *bitBlockType << "\n"; // for testing
-            llvm::errs() << "getPrimitiveSizeInBits: " << bitBlockType->getPrimitiveSizeInBits() << "\n"; // for testing
-            llvm::errs() << "getInt8(0x20): " << *b.getInt8(0x20) << "\n"; // for testing
+        // Create output stream blocks
+        Value * outputBlock1 = b.CreateVectorSplat(bitBlockType->getPrimitiveSizeInBits() / 8, b.getInt8(0));
+        Value * outputBlock2 = b.CreateVectorSplat(bitBlockType->getPrimitiveSizeInBits() / 8, b.getInt8(0));
 
-            Value * uppercaseMask = b.CreateVectorSplat(bitBlockType->getPrimitiveSizeInBits() / 8, b.getInt8(0xDF));
+        // Insert 'a' and extra 'a's in the output blocks
+        b.CreateInsertElement(outputBlock1, b.getInt8('a'), b.getInt32(0));
+        b.CreateInsertElement(outputBlock2, b.getInt8('a'), b.getInt32(1));
+        b.CreateInsertElement(outputBlock2, b.getInt8('a'), b.getInt32(2));
 
-            // Check if characters are lowercase
-            Value * isLowercase = b.CreateICmpEQ(b.CreateAnd(inputBlock, lowercaseMask), lowercaseMask);
+        // Conditionally store the output blocks based on 'isA' mask
+        Value * output = b.CreateSelect(isA, outputBlock2, outputBlock1);
 
-            // Calculate uppercase values
-            Value * uppercaseBlock = b.CreateOr(
-                b.CreateAnd(inputBlock, uppercaseMask), 
-                b.CreateAnd(isLowercase, lowercaseMask)
-            );
-
-            // Store output block
-            b.storeOutputStreamBlock("outputStream", b.getInt32(0), b.getInt32(i), uppercaseBlock);
-        }
+        // Store output block
+        b.storeOutputStreamBlock("outputStream", b.getInt32(0), b.getInt32(0), output);
     }
 };
-
 
 typedef void (*TransliteratorFunctionType)(uint32_t fd);
 
@@ -87,16 +62,13 @@ TransliteratorFunctionType transliterator_gen(CPUDriver & driver) {
     // Source data
     StreamSet * const codeUnitStream = P->CreateStreamSet(1, 8);
     P->CreateKernelCall<ReadSourceKernel>(fileDescriptor, codeUnitStream);
-    SHOW_BYTES(codeUnitStream);
 
-
-    // Uppercase transformation
-    StreamSet * const upperStream = P->CreateStreamSet(1, 8);
-    P->CreateKernelCall<UppercaseKernel>(codeUnitStream, upperStream);
+    // Insert extra 'a' kernel transformation
+    StreamSet * const outputStream = P->CreateStreamSet(1, 8);
+    P->CreateKernelCall<InsertExtraAKernel>(codeUnitStream, outputStream);
 
     // Output
-    P->CreateKernelCall<StdOutKernel>(upperStream);
-    SHOW_BYTES(upperStream);
+    P->CreateKernelCall<StdOutKernel>(outputStream);
 
     return reinterpret_cast<TransliteratorFunctionType>(P->compile());
 }
