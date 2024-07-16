84a2564f49f7b3dc75eed4b5546ea02f6e54a119
hla294 <hla294@csil-cpu06.csil.sfu.ca>
2024-07-15 17:21:43 -0700

    create hangulhalftofull that transforms half hangul character to full width character. it can compile but does not produce output yet. still in work on this

--- tools/util/hangulhalftofull.cpp
+++ tools/util/hangulhalftofull.cpp
@@ -1,4 +1,3 @@
-
 #include <cstdio>
 #include <vector>
 #include <llvm/Support/CommandLine.h>
@@ -44,6 +43,7 @@
 #include <unicode/utf/transchar.h>
 #include <codecvt>
 #include <re/toolchain/toolchain.h>
+#include <unordered_map>
 
 #define SHOW_STREAM(name) if (codegen::EnableIllustrator) P->captureBitstream(#name, name)
 #define SHOW_BIXNUM(name) if (codegen::EnableIllustrator) P->captureBixNum(#name, name)
@@ -53,10 +53,52 @@ using namespace kernel;
 using namespace llvm;
 using namespace pablo;
 
-//  These declarations are for command line processing.
-//  See the LLVM CommandLine 2.0 Library Manual https://llvm.org/docs/CommandLine.html
-static cl::OptionCategory LatinHalfToFullOptions("latinhalftofull Options", "latinhalftofull control options.");
-static cl::opt<std::string> inputFile(cl::Positional, cl::desc("<input file>"), cl::Required, cl::cat(LatinHalfToFullOptions));
+std::unordered_map<UCD::codepoint_t, UCD::codepoint_t> halfwidthToFullwidthHangulMap = {
+    {0xFFA1, 0x1100}, //ㄱ
+    {0xFFA2, 0x1101}, //ㄲ
+    {0xFFA4, 0x1102}, //ㄴ
+    {0xFFA7, 0x1103}, //ㄷ
+    {0xFFA8, 0x1104}, //ㄸ
+    {0xFFA9, 0x1105}, //ㄹ
+    {0xFFB1, 0x1106}, //ㅁ
+    {0xFFB2, 0x1107}, //ㅂ
+    {0xFFB3, 0x1108}, //ㅃ
+    {0xFFB5, 0xFFB5}, //ㅅ
+    {0xFFB6, 0x110A}, //ㅆ
+    {0xFFB7, 0x110B}, //ㅇ
+    {0xFFB8, 0x110C}, //ㅈ
+    {0xFFB9, 0x110D}, //ㅉ
+    {0xFFBA, 0x110E}, //ㅊ
+    {0xFFBB, 0x110F}, //ㅋ
+    {0xFFBC, 0x1110}, //ㅌ
+    {0xFFBD, 0x1111}, //ㅍ
+    {0xFFBE, 0x1112}, //ㅎ
+    {0xFFC2, 0x1161}, //ㅏ
+    {0xFFC3, 0x1162}, //ㅐ
+    {0xFFC4, 0x1163}, //ㅑ
+    {0xFFC5, 0x1164}, //ㅒ
+    {0xFFC6, 0x1165}, //ㅓ
+    {0xFFC7, 0x1166}, //ㅔ
+    {0xFFCA, 0x1167}, //ㅕ
+    {0xFFCB, 0x1168}, //ㅖ
+    {0xFFCC, 0x1169}, //ㅗ
+    {0xFFCD, 0x116A}, //ㅘ
+    {0xFFCE, 0x116B}, //ㅙ
+    {0xFFCF, 0x116C}, //ㅚ
+    {0xFFD2, 0x116D}, //ㅛ
+    {0xFFD3, 0x116E}, //ㅜ
+    {0xFFD4, 0x116F}, //ㅝ
+    {0xFFD5, 0x1170}, //ㅞ
+    {0xFFD6, 0x1171}, //ㅟ
+    {0xFFD7, 0x1172}, //ㅠ
+    {0xFFDA, 0x1173}, //ㅡ
+    {0xFFDB, 0x1174}, //ㅢ
+    {0xFFDC, 0x1175}, //ㅣ
+};
+
+
+static cl::OptionCategory HangulHalfToFullOptions("hangulhalftofull Options", "hangulhalftofull control options.");
+static cl::opt<std::string> inputFile(cl::Positional, cl::desc("<input file>"), cl::Required, cl::cat(HangulHalfToFullOptions));
 
 class FullWidthIfy : public pablo::PabloKernel {
 public:
@@ -69,40 +111,30 @@ protected:
 };
 
 void FullWidthIfy::generatePabloMethod() {
-    //  pb is an object used for build Pablo language statements
     pablo::PabloBuilder pb(getEntryScope());
-
-    //  bnc is an object that can perform arithmetic on sets of parallel bit streams
-    BixNumCompiler bnc(pb);
-
-    // Get the input stream sets.
-    //PabloAST * halfwidths = getInputStreamSet("halfwidths")[0];
     std::vector<PabloAST *> U21 = getInputStreamSet("U21");
-
-    // ccc is an object that can compile character classes from a set of 8 parallel bit streams.
     cc::Parabix_CC_Compiler_Builder ccc(getEntryScope(), U21);
 
-    // character class for latin halfwidths
-    UCD::codepoint_t low_cp = 0x0021;
-    UCD::codepoint_t hi_cp = low_cp + 105;
-    PabloAST * halfwidths = ccc.compileCC(re::makeCC(low_cp, hi_cp, &cc::Unicode));
+    std::vector<PabloAST *> fullwidthStreams(21, nullptr);
+    for (unsigned i = 0; i < 21; i++) {
+        fullwidthStreams[i] = pb.createZeroes();
+    }
 
-    // the gap between half and fullwidth latin characters
-    UCD::codepoint_t latinGap = 0xFEE0;
+    for (auto &entry : halfwidthToFullwidthHangulMap) {
+        UCD::codepoint_t halfwidthCodepoint = entry.first;
+        UCD::codepoint_t fullwidthCodepoint = entry.second;
+        PabloAST * halfwidthPattern = ccc.compileCC(re::makeCC(halfwidthCodepoint, halfwidthCodepoint, &cc::Unicode));
 
-    // For anything other than latin, likely will have to use a map
-    UCD::codepoint_t whiteParenGap = 0xD5DA;
-    UCD::codepoint_t ideoStopGap = 0xCF5F;
-    UCD::codepoint_t cornerBrakGap = 0xCF56;
-    UCD::codepoint_t ideoCommaGap = 0xCF63;
-    
-
-    BixNum basisVar = bnc.AddModular(U21, latinGap);
+        for (unsigned i = 0; i < 21; i++) {
+            PabloAST * fullwidthBitPattern = ccc.compileCC(re::makeCC((fullwidthCodepoint >> i) & 1, (fullwidthCodepoint >> i) & 1, &cc::Unicode));
+            PabloAST * bit = pb.createAnd(halfwidthPattern, fullwidthBitPattern);
+            fullwidthStreams[i] = pb.createOr(fullwidthStreams[i], bit);
+        }
+    }
 
     Var * fullWidthBasisVar = getOutputStreamVar("fullWidthBasis");
     for (unsigned i = 0; i < 21; i++) {
-
-        pb.createAssign(pb.createExtract(fullWidthBasisVar, pb.getInteger(i)), pb.createSel(halfwidths, basisVar[i], U21[i]));
+        pb.createAssign(pb.createExtract(fullWidthBasisVar, pb.getInteger(i)), fullwidthStreams[i]);
     }
 }
 
@@ -110,26 +142,18 @@ void FullWidthIfy::generatePabloMethod() {
 typedef void (*HalfToFullFunctionType)(uint32_t fd);
 
 HalfToFullFunctionType generatePipeline(CPUDriver & pxDriver) {
-    // A Parabix program is build as a set of kernel calls called a pipeline.
-    // A pipeline is construction using a Parabix driver object.
     auto & b = pxDriver.getBuilder();
     auto P = pxDriver.makePipeline({Binding{b.getInt32Ty(), "inputFileDecriptor"}}, {});
 
-    //  The program will use a file descriptor as an input.
     Scalar * fileDescriptor = P->getInputScalar("inputFileDecriptor");
     StreamSet * ByteStream = P->CreateStreamSet(1, 8);
-
-    //  ReadSourceKernel is a Parabix Kernel that produces a stream of bytes
-    //  from a file descriptor.
     P->CreateKernelCall<ReadSourceKernel>(fileDescriptor, ByteStream);
     SHOW_BYTES(ByteStream);
 
-    // Get the basis bits
     StreamSet * BasisBits = P->CreateStreamSet(8, 1);
     P->CreateKernelCall<S2PKernel>(ByteStream, BasisBits);
     SHOW_BIXNUM(BasisBits);
 
-    // Convert into codepoints
     StreamSet * u8index = P->CreateStreamSet(1, 1);
     P->CreateKernelCall<UTF8_index>(BasisBits, u8index);
     SHOW_STREAM(u8index);
@@ -141,15 +165,13 @@ HalfToFullFunctionType generatePipeline(CPUDriver & pxDriver) {
     FilterByMask(P, u8index, U21_u8indexed, U21);
     SHOW_BIXNUM(U21);
 
-    // Perform the logic of the FullWidthIfy kernel on the codepoiont values.
     StreamSet * fullWidthBasis = P->CreateStreamSet(21, 1);
     P->CreateKernelCall<FullWidthIfy>(U21, fullWidthBasis);
     SHOW_BIXNUM(fullWidthBasis);
 
-    // Convert back to UTF8 from codepoints.
     StreamSet * const OutputBasis = P->CreateStreamSet(8);
     U21_to_UTF8(P, fullWidthBasis, OutputBasis);
-
+    
     SHOW_BIXNUM(OutputBasis);
 
     StreamSet * OutputBytes = P->CreateStreamSet(1, 8);
@@ -162,10 +184,10 @@ HalfToFullFunctionType generatePipeline(CPUDriver & pxDriver) {
 int main(int argc, char *argv[]) {
     //  ParseCommandLineOptions uses the LLVM CommandLine processor, but we also add
     //  standard Parabix command line options such as -help, -ShowPablo and many others.
-    codegen::ParseCommandLineOptions(argc, argv, {&LatinHalfToFullOptions, pablo::pablo_toolchain_flags(), codegen::codegen_flags()});
+    codegen::ParseCommandLineOptions(argc, argv, {&HangulHalfToFullOptions, pablo::pablo_toolchain_flags(), codegen::codegen_flags()});
 
     //  A CPU driver is capable of compiling and running Parabix programs on the CPU.
-    CPUDriver driver("latinhalftofull");
+    CPUDriver driver("hangulhalftofull");
 
     //  Build and compile the Parabix pipeline by calling the Pipeline function above.
     HalfToFullFunctionType fn = generatePipeline(driver);
