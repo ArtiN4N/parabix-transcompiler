51697e884b45dbc3655871e20ac09c87822b0848
Julian <julian.mjm.300@gmail.com>
2024-07-17 15:24:50 -0700

    fixed bugs in upper transform

--- tools/util/upper.cpp
+++ tools/util/upper.cpp
@@ -11,6 +11,58 @@
 #include <llvm/IR/Module.h>
 #include <re/adt/re_name.h>
 #include <re/adt/re_re.h>
+#include <re/adt/re_range.h>
+#include <kernel/core/kernel_builder.h>
+#include <kernel/pipeline/pipeline_builder.h>
+#include <kernel/streamutils/deletion.h>
+#include <kernel/streamutils/pdep_kernel.h>
+#include <kernel/streamutils/stream_select.h>
+#include <kernel/streamutils/stream_shift.h>
+#include <kernel/basis/s2p_kernel.h>
+#include <kernel/basis/p2s_kernel.h>
+#include <kernel/io/source_kernel.h>
+#include <kernel/io/stdout_kernel.h>
+#include <kernel/scan/scanmatchgen.h>
+#include <re/adt/re_name.h>
+#include <re/cc/cc_kernel.h>
+#include <re/cc/cc_compiler.h>
+#include <re/transforms/name_intro.h>
+#include <re/transforms/re_transformer.h>
+#include <re/cc/cc_compiler_target.h>
+#include <re/unicode/resolve_properties.h>
+#include <string>
+#include <toolchain/toolchain.h>
+#include <pablo/pablo_toolchain.h>
+#include <pablo/builder.hpp>
+#include <pablo/pe_ones.h>
+#include <pablo/pe_zeroes.h>
+#include <pablo/bixnum/bixnum.h>
+#include <fcntl.h>
+#include <iostream>
+#include <kernel/pipeline/driver/cpudriver.h>
+
+#include <kernel/unicode/charclasses.h>
+#include <kernel/unicode/utf8gen.h>
+#include <kernel/unicode/utf8_decoder.h>
+#include <kernel/unicode/UCD_property_kernel.h>
+#include <unicode/algo/decomposition.h>
+
+#include <unicode/core/unicode_set.h>
+
+#include <unicode/data/PropertyAliases.h>
+#include <unicode/data/PropertyObjects.h>
+#include <unicode/data/PropertyObjectTable.h>
+#include <unicode/utf/utf_compiler.h>
+#include <unicode/utf/transchar.h>
+
+#include <cstdio>
+#include <vector>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/ErrorHandling.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/IR/Module.h>
+#include <re/adt/re_name.h>
+#include <re/adt/re_re.h>
 #include <pablo/codegenstate.h>
 #include <pablo/pe_zeroes.h>        // for Zeroes
 #include <pablo/bixnum/bixnum.h>
