20f1ea56b1c6cadcd8be71c0a6ad5f737691f4d5
Julian <julian.mjm.300@gmail.com>
2024-07-20 15:14:36 -0700

    fixing regex to be correct for title transform

--- transcompiler/transforms/title.cpp
+++ transcompiler/transforms/title.cpp
@@ -86,7 +86,7 @@ void Titleify::generatePabloMethod() {
 
     std::cout << "doing regex" << std::endl;
     // "(^|\\s)(.)"
-    re::RE * CC_re = re::simplifyRE(re::RE_Parser::parse("(.)"));
+    re::RE * CC_re = re::simplifyRE(re::RE_Parser::parse("[a-p]"));
     std::cout << "doing link" << std::endl;
     CC_re = UCD::linkAndResolve(CC_re);
     std::cout << "doing externalize" << std::endl;
@@ -94,7 +94,7 @@ void Titleify::generatePabloMethod() {
 
     std::cout << "doing recast" << std::endl;
     re::CC * titlePositions_CC = dyn_cast<re::CC>(CC_re);
-    std::cout << "compiled regex" << std::endl;
+    std::cout << "compiling regex" << std::endl;
 
     PabloAST * regex = ccc.compileCC(titlePositions_CC);
 
