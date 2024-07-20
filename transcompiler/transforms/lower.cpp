d9f7e0e9347d936e6f552530e0f218f94d9f50c8
Julian <julian.mjm.300@gmail.com>
2024-07-20 13:50:03 -0700

    testing removing uncessary includes

--- transcompiler/transforms/lower.cpp
+++ transcompiler/transforms/lower.cpp
@@ -1,49 +1,61 @@
 #include <vector>
 
 #include <pablo/codegenstate.h>
-#include <pablo/pe_zeroes.h>        // for Zeroes
-#include <pablo/bixnum/bixnum.h>
-/*
-#include <cstdio>
-
+//#include <pablo/pe_zeroes.h>        // for Zeroes
+//#include <pablo/bixnum/bixnum.h>
 #include <llvm/Support/CommandLine.h>
 #include <llvm/Support/ErrorHandling.h>
 #include <llvm/Support/raw_ostream.h>
+
+#include <kernel/pipeline/driver/cpudriver.h>
+#include <kernel/core/kernel_builder.h>
+#include <kernel/pipeline/pipeline_builder.h>
+#include <kernel/basis/s2p_kernel.h>
+#include <kernel/basis/p2s_kernel.h>
+#include <kernel/io/source_kernel.h>
+#include <kernel/io/stdout_kernel.h>
+
+#include <re/cc/cc_kernel.h>
+#include <re/cc/cc_compiler.h>
+#include <re/cc/cc_compiler_target.h>
+
+#include <unicode/data/PropertyObjects.h>
+#include <unicode/data/PropertyObjectTable.h>
+
+
+/*
+#include <cstdio>
+
+
 #include <llvm/IR/Module.h>
 #include <re/adt/re_name.h>
 #include <re/adt/re_re.h>
 
 #include <grep/grep_kernel.h>
-#include <kernel/core/kernel_builder.h>
-#include <kernel/pipeline/pipeline_builder.h>
+
 #include <kernel/streamutils/deletion.h>
 #include <kernel/streamutils/pdep_kernel.h>
 #include <kernel/streamutils/run_index.h>
 #include <kernel/streamutils/string_insert.h>
-#include <kernel/basis/s2p_kernel.h>
-#include <kernel/basis/p2s_kernel.h>
-#include <kernel/io/source_kernel.h>
-#include <kernel/io/stdout_kernel.h>
+
+
 #include <kernel/unicode/charclasses.h>
 #include <kernel/unicode/utf8gen.h>
 #include <kernel/unicode/utf8_decoder.h>
 #include <kernel/unicode/UCD_property_kernel.h>
 #include <re/adt/re_name.h>
-#include <re/cc/cc_kernel.h>
-#include <re/cc/cc_compiler.h>
-#include <re/cc/cc_compiler_target.h>
+
 #include <string>
 #include <toolchain/toolchain.h>
 #include <pablo/pablo_toolchain.h>
 #include <fcntl.h>
-#include <kernel/pipeline/driver/cpudriver.h>
+
 #include <unicode/algo/decomposition.h>
 #include <unicode/core/unicode_set.h>
 #include <unicode/data/CaseFolding.h>
 #include <unicode/data/Equivalence.h>
 #include <unicode/data/PropertyAliases.h>
-#include <unicode/data/PropertyObjects.h>
-#include <unicode/data/PropertyObjectTable.h>
+
 #include <unicode/utf/utf_compiler.h>
 #include <unicode/utf/transchar.h>
 #include <codecvt>
