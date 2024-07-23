d55dfe2b44590710c4238cae4ccb1c029aa8a5d9
Julian <julian.mjm.300@gmail.com>
2024-07-22 19:09:44 -0700

    fixed ucount

--- tools/wc/ucount.cpp
+++ tools/wc/ucount.cpp
@@ -7,6 +7,7 @@
 #include <boost/filesystem.hpp>
 #include <re/cc/cc_compiler.h>
 #include <re/cc/cc_compiler_target.h>
+#include <re/transforms/re_simplifier.h>
 #include <re/adt/adt.h>
 #include <re/parse/parser.h>
 #include <re/unicode/resolve_properties.h>
