195c4fa0259aac9b28302893be4dde35e1e76326
Julian <julian.mjm.300@gmail.com>
2024-07-28 02:40:38 -0700

    further generallized lascii transform

--- transcompiler/transforms/replaceify_kernel.h
+++ transcompiler/transforms/replaceify_kernel.h
@@ -3,11 +3,15 @@
 #include "replace_bixData.h"
 #include <pablo/pablo_kernel.h>
 
+#include <kernel/core/kernel.h>
+#include <llvm/IR/Value.h>
+#include <string>
+#include <kernel/pipeline/driver/driver.h>
+#include <kernel/pipeline/pipeline_builder.h>
+
+
 using namespace kernel;
 using namespace llvm;
 using namespace pablo;
 
-void ReplaceByBixData(PipelineBuilder & P,
-                  replace_bixData & BixData,    
-                  StreamSet * Basis, StreamSet * Output
-                  );
+void ReplaceByBixData(PipelineBuilder & P, replace_bixData & BixData, StreamSet * Basis, StreamSet * Output);
