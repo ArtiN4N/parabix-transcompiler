802b4f9fcb0bb74c5d15ff41e5cb14a9aea646a7
Julian <julian.mjm.300@gmail.com>
2024-07-28 02:19:02 -0700

    further generallized lascii transform

--- transcompiler/transforms/replaceify_kernel.h
+++ transcompiler/transforms/replaceify_kernel.h
@@ -10,9 +10,30 @@
 #include <unicode/utf/utf_compiler.h>
 #include <unicode/utf/transchar.h>
 
+#include <kernel/streamutils/deletion.h>
+#include <kernel/streamutils/string_insert.h>
+#include <kernel/streamutils/pdep_kernel.h>
+#include <kernel/pipeline/driver/cpudriver.h>
+#include <kernel/pipeline/pipeline_builder.h>
+#include <kernel/unicode/UCD_property_kernel.h>
+#include <kernel/unicode/utf8_decoder.h>
+#include <kernel/unicode/charclasses.h>
+#include <kernel/unicode/utf8gen.h>
+#include <kernel/basis/s2p_kernel.h>
+#include <kernel/basis/p2s_kernel.h>
+#include <kernel/core/kernel_builder.h>
+#include <kernel/io/source_kernel.h>
+#include <kernel/io/stdout_kernel.h>
+
 #include <pablo/codegenstate.h>
 #include <pablo/pe_zeroes.h>        // for Zeroes
 #include <pablo/bixnum/bixnum.h>
+
+#include <grep/grep_kernel.h>
+
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/ErrorHandling.h>
+#include <llvm/Support/raw_ostream.h>
 
 #include <re/toolchain/toolchain.h>
 #include <re/adt/re_name.h>
