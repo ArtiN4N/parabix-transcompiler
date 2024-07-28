97b820b13f7cb3837239d24a54372d972639bc3b
Julian <julian.mjm.300@gmail.com>
2024-07-28 02:43:09 -0700

    further generallized lascii transform

--- transcompiler/transforms/replaceify_kernel.h
+++ transcompiler/transforms/replaceify_kernel.h
@@ -15,3 +15,11 @@ using namespace llvm;
 using namespace pablo;
 
 void ReplaceByBixData(PipelineBuilder & P, replace_bixData & BixData, StreamSet * Basis, StreamSet * Output);
+
+inline void ReplaceByBixData(const std::unique_ptr<PipelineBuilder> & P, replace_bixData & BixData, StreamSet * Basis, StreamSet * Output) {
+    return ReplaceByBixData(*P.get(), BixData, Basis, Output);
+}
+
+inline void ReplaceByBixData(const std::unique_ptr<ProgramBuilder> & P, replace_bixData & BixData, StreamSet * Basis, StreamSet * Output) {
+    return ReplaceByBixData(*P.get(), BixData, Basis, Output);
+}
