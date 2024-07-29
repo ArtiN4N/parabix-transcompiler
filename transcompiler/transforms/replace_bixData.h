8086f6e6c93df3c03e50a603cbc2a7f8656f9236
Julian <julian.mjm.300@gmail.com>
2024-07-28 20:36:15 -0700

    fixing bugs in lascii transform

--- transcompiler/transforms/replace_bixData.h
+++ transcompiler/transforms/replace_bixData.h
@@ -16,8 +16,9 @@
 #include <re/cc/cc_compiler.h>
 #include <re/cc/cc_kernel.h>
 
-template <std::size_t N>;
+
 struct replace_bixData {
+    template <std::size_t N>
     replace_bixData(std::array<std::pair<UCD::codepoint_t, std::vector<UCD::codepoint_t>>, N>);
     std::vector<re::CC *> insertionBixNumCCs();
     unicode::BitTranslationSets matchBitXorCCs(unsigned);
