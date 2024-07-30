1932b39a140a2f0d69b94a58e19083421e0ca142
Julian <julian.mjm.300@gmail.com>
2024-07-30 04:47:41 -0700

    renamed headers

--- transcompiler/transforms/data/lowernamesA.py
+++ transcompiler/transforms/data/lowernamesA.py
@@ -12,11 +12,16 @@ for filename in os.listdir(directory):
     if os.path.isdir(old_file):
         continue
     
+    print("hi")
     # Generate the new file name with lowercase letters
     new_filename = filename.lower()
+    name, ext = os.path.splitext(new_filename)
+    
+    new_filename = name + "A" + ext
+    new_file = os.path.join(directory, new_filename)
     
     # Create the full new file path
-    new_file = os.path.join(directory, new_filename)
+    #new_file = os.path.join(directory, new_filename)
     
     # Rename the file
     os.rename(old_file, new_file)
