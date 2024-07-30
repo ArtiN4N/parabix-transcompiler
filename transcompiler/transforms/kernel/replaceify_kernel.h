c5d2d2d399be38ef37d699a2c3104378edb00467
Julian <julian.mjm.300@gmail.com>
2024-07-30 07:45:17 -0700

    continued development of compiler

--- transcompiler/transforms/kernel/replaceify_kernel.h
+++ transcompiler/transforms/kernel/replaceify_kernel.h
@@ -67,12 +67,11 @@ replace_bixData::replace_bixData(std::array<std::pair<UCD::codepoint_t, std::vec
     maxAdd = 0;
     for (auto& pair : data) {
         mInsertLength.emplace(pair.first, pair.second.size());
-        unsigned oldSize = pair.second.size();
+        unsigned int oldSize = pair.second.size();
         if (pair.second.size() > maxAdd) {
             maxAdd++;
-            for (int i = 0; i < (maxAdd - oldSize); i++)
+            for (unsigned int i = 0; i < (maxAdd - oldSize); i++)
                 mCharMap.push_back({});
-            std::cout << mCharMap.size() << std::endl;
         }
 
         unsigned int i = 0;
