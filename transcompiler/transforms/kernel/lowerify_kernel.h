6981f4ed8fb4d893cbbac0ae4b7e704af1ba13a4
Julian <julian.mjm.300@gmail.com>
2024-08-06 09:47:54 -0700

    removed debug statement

--- transcompiler/transforms/kernel/lowerify_kernel.h
+++ transcompiler/transforms/kernel/lowerify_kernel.h
@@ -62,7 +62,6 @@ protected:
 };
 
 void Lowerify::generatePabloMethod() {
-    std::cout << "called lowerify" << std::endl;
     //  pb is an object used for build Pablo language statements
     pablo::PabloBuilder pb(getEntryScope());
 
